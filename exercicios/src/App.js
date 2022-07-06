97a2bb75fae233ba9160937047f638eaa90b3eee
Heberty <hebertyprado22005@gmail.com>
2022-07-06 12:30:19 -0300

    att

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
+import Botao from './components/Botao'
+import Titulo from './components/Titulo'
 import MinMax from './components/MinMax'
 import Aleatorio from './components/Aleatorio'
 import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 import Primeiro from './components/Primeiro'
+import Contador from './components/Contador'
+import Pai from './components/indireta/Pai'
+import ContadorV2 from './components/contador/ContadorV2'
+import Diferenciar from './components/Diferenciar'
 
 export default () => (
     <View style={style.App}>
+
+        <Diferenciar />
+        
+        {/*
+        <ContadorV2 />
+        <Pai />
+        <Contador inicial={100} passo={13} />
+        <Contador />
+        <Botao />
+        <Titulo principal="Cadastro Produto"
+            segundario="Tela de Cadastro do Produto" />
         <MinMax min={5} max={80} />
         <Aleatorio number1={1} number2={90} />
         <Aleatorio number1={1} number2={90} />
         <Aleatorio number1={1} number2={90} />
-
-
-        {/*
         <CompPadrao />
         <Comp1 />
         <Comp2 />
--- exercicios/src/components/Aleatorio.js
+++ exercicios/src/components/Aleatorio.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Text } from 'react-native'
 import Estilo from './estilo'
 
-export default (props) => {
-    const aleatorio = Math.floor((Math.random() * (props.number1-props.number2)) + props.number2);
+export default ({ number1, number2 }) => {
+    const aleatorio = Math.floor((Math.random() * (number1-number2)) + number2);
 
     return (
         <Text style={Estilo.txtG}>
-            Número aleatório entre {props.number1} e {props.number2} é: {aleatorio}
+            Número aleatório entre {number1} e {number2} é: {aleatorio}
         </Text>
     )
 }
